8e8f6de160eecd4484fff7179bb701a2321d5608
Abdul Basit <127122706+Adrein10@users.noreply.github.com>
2024-09-21 16:40:31 +0500

    learning Dart language

---
+++ Dart/main.dart
@@ -0,0 +1,34 @@
+
+
+void main(){
+// // var can initialize any type of values and can be initialize more than once
+//     var name;
+//     name = "abc";
+// //    print(name);
+
+// // final can initialize any type of values and can be initialize only one time
+//     final name2;
+//     name2 = "def";
+// //    print(name2);
+
+// // const can initialize any type of values and can be initialize only one time
+//     const name3 = "abcdef";
+// //    print(name3);
+
+
+// int a = 9;
+// String name4 = "abcdef";
+// bool can = true;
+// double perc = 10.58;
+// // print(a);
+// // print(name4);
+
+// // print(can);
+
+// // print(perc);
+
+List<String> array = ["name1","name2","name3","name4"];
+for (int i = 0; i < array.length; i++){
+  print(array[i]);
+}
+}
